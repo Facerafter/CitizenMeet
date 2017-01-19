3894c4a0b8f9e24b77c460a3517dfda70ea4f26e
Facerafter <bouman0707@gmail.com>
2017-01-19 22:04:50 +0100

    updated map controller

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -10,6 +10,6 @@ class MapController extends Controller
   public function index()
   {
     Mapper::map(53.381128999999990000, -1.470085000000040000);
-    return view('map')
+    return view('eventmap')
   }
 }
