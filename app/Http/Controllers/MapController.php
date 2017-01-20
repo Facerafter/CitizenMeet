6fe791dc5c216b515cf8ba5289506e86b5cccf47
Julian Bouman <bouman0707@gmail.com>
2017-01-20 15:40:55 +0100

    map marker test

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 
 use Cornford\Googlmapper\Facades\MapperFacade as Mapper;
+use App\MyEvent;
 
 class MapController extends Controller
 {
@@ -9,6 +10,11 @@ class MapController extends Controller
     public function index()
     {
         Mapper::map(38.610398238551156, -28.256835935000026,['zoom'=>3, 'marker'=>false]);
+        $events = MyEvent::where('approved', 1)->where('expired', 0)->get();
+        foreach ($events as $event) {
+          Mapper::location(MyEvent::getFullAddress)->marker();
+        }
+        foreachMyEvent::find($id)->delete();
 
         return view('eventmap');
     }
--- app/MyEvent.php
+++ app/MyEvent.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class MyEvent extends Model
 {
     protected $table = 'events';
+
+    public function getFullAddress()
+    {
+      return $this->address . " " . $this->city;
+    }
 }
