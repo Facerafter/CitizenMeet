740aa4383271b665e8cc6e544a1c49183cba0fd2
Facerafter <bouman0707@gmail.com>
2017-01-20 01:37:09 +0100

    updated eventmap to use bootstrap

--- resources/views/eventmap.blade.php
+++ resources/views/eventmap.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 @section('content')
   <div class="row">
-    <div class="col-md-12">
-      <div class="panel panel-primary">
-        <div class="panel-body">
-          {!! Mapper::render() !!}
-        </div>
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <div class="embed-responsive embed-responsive-16by9">
+              <div id="map_canvas" class="embed-responsive-item">
+                  {!! Mapper::render () !!}
+              </div>
+          </div>
       </div>
-    </div>
   </div>
 @endsection
