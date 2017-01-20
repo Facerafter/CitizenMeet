a9a2205239579e17e0575226e623f7e722e930f5
Julian Bouman <bouman0707@gmail.com>
2017-01-20 09:34:18 +0100

    updated maprender

--- resources/views/eventmap.blade.php
+++ resources/views/eventmap.blade.php
@@ -5,7 +5,7 @@
     <h1 class="page-header">Event Worldmap</h1>
     <ol class="breadcrumb">
       <li><a href="{{ url('/') }}">Home</a></li>
-      <li class="active">Event Map</li>
+      <li class="active">Event Worldmap</li>
     </ol>
   </div>
 </div>
@@ -13,7 +13,7 @@
   <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
     <div class="embed-responsive embed-responsive-16by9">
       <div id="map_canvas" class="embed-responsive-item">
-        {!! Mapper::render ()!!}
+        {!! Mapper::render() !!}
       </div>
     </div>
   </div>
