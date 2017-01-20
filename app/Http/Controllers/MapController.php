db77eb3f4adce8f4886eb16de528d0e463d3a087
Julian Bouman <bouman0707@gmail.com>
2017-01-20 16:27:35 +0100

    mapmarker fix

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -10,9 +10,9 @@ class MapController extends Controller
     public function index()
     {
         Mapper::map(38.610398238551156, -28.256835935000026,['zoom'=>3, 'marker'=>false]);
-        $events = MyEvent::where('approved', 1)->where('expired', 0)->get();
+        $events = MyEvent::getFullAddress->where('approved', 1)->where('expired', 0)->get();
         foreach ($events as $event) {
-          Mapper::location(MyEvent::getFullAddress)->marker();
+          Mapper::location($event)->marker();
         }
         foreachMyEvent::find($id)->delete();
 
